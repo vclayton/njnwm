f77e5ab77e300defa0e66273f2cc20a1d2c84850
Vaughn Clayton <vaughn.clayton+github@gmail.com>
2012-04-16 17:39:58 -0400

    Refactored, but only slightly.

--- njnwm.js
+++ njnwm.js
@@ -1,6 +1,7 @@
 
 var Buffer = require('buffer').Buffer;
 
+var EventEmitter = require('events').EventEmitter;
 var Future = require('fibers/future'), wait = Future.wait;
 var Fiberize = require('fiberize');
 var Promise = require('fibers-promise');
@@ -13,142 +14,135 @@ var wid, cidBlack, cidWhite;
 var X;
 var Window = require('./lib/window.js');
 
-function draw()
-{
-	var whiterects = [];
-	var blackrects = [];
-	for (var x=0; x < cupsize[0]; ++x)
-	{
-		for (var y=0; y < cupsize[1]; ++y)
-		{
-			var index = x + y*cupsize[0];
-			var rect = [x*sqsize, (cupsize[1]-1)*sqsize - y*sqsize, sqsize, sqsize];
-			if (cup[index] != 0)
-				blackrects = blackrects.concat(rect);
-			else
-				whiterects = whiterects.concat(rect);
-		}
+
+//var WindowManager = {
+var WindowManager = function() {
+	var self = this;
+	self.clients = [];
+	self.frames = {};
+	self.X = undefined;
+
+	self.xinit = function xinit(xserver) {
+		console.log(this);
+
+
+		self.xserver = xserver;
+		self.X = xserver.client;
+		self.root = xserver.screen[0].root;
+		self.getAtomName = Future.wrap(self.X.GetAtomName, 1);
+
+		self.setupDisplay();
+		self.scanWindows();
+
+		// Do Event Loop
+		self.X.on('error', function(err) {
+			console.log("Error: ", err);
+		}).on('event', function(ev) {
+			console.log("Event: ", ev);
+		});
+		console.log("xinit done");
 	}
-	var fig = getTransformedFigure(fignum, angle, pos);
-	for (var i=0; i < fig.length; i+=2)
-	{
-		var x = fig[i];
-		var y = fig[i+1]
-		blackrects = blackrects.concat([x*sqsize, (cupsize[1]-1)*sqsize - y*sqsize, sqsize, sqsize]);
+
+	self.setupDisplay = function setupDisplay() {
+		// Here we will eventually also set all of our extended window manager hints for apps to use
+
+		// Define cursor
+
+		// Root event mask
+		self.X.ChangeWindowAttributes( {window: self.root, value_mask: { 
+			EventMask: x11.eventMask.SubstructureRedirect | x11.eventMask.SubstructureNotifyMask }}, function(err) {
+				if (err.error == 10) // @todo Named Error constant
+				{
+					console.error('Error: another window manager already running.');
+					process.exit(1);
+				}
+		});
+
+		// Grab keys for root window
+		console.log("setupDisplay done");
 	}
-	X.PolyFillRectangle(wid, cidWhite, whiterects);
-	X.PolyFillRectangle(wid, cidBlack, blackrects);
+
+	self.scanWindows = function scanWindows() {
+		self.X.QueryTree({window: self.root}, function(tree) {
+			console.log("Children tree: ", tree);
+			tree.children.forEach(self.manageWindow);
+		});
+		console.log("scanWindows done");
+	}
+
+	self.manageWindow = function manageWindow(wid) {
+		console.log("Managing window "+wid);
+		var events = x11.eventMask.Button1Motion|x11.eventMask.ButtonPress|x11.eventMask.ButtonRelease|x11.eventMask.SubstructureNotify|x11.eventMask.SubstructureRedirect;
+
+		self.X.GetWindowAttributes({window: wid}, function(attrs) {
+			console.log("Got attributes:", attrs);
+			if (false && attrs.map_state) // @todo Named attribute mask constants
+			{
+				console.log("Attribute 8?", attrs);
+				self.X.MapWindow({window: wid});
+				return;
+			}
+
+			var fid = self.X.AllocID();
+			self.frames[fid] = 1;
+			console.log("Frames: ", self.frames);
+			var winX, winY;
+			var dragStart;
+			winX = parseInt(Math.random()*300);
+			winY = parseInt(Math.random()*300);
+			
+			self.X.GetGeometry({drawable: wid}, function(clientGeom) {
+				var width = clientGeom.width + 4;
+				var height = clientGeom.height + 24;
+				self.X.CreateWindow({wid: fid, parent: self.root, x:winX, y:winY, width:width, height:height, border_width:1, _class:1, visual:0,
+					value_mask: { BackPixel: 0xffffe0, EventMask: events } });
+
+				var ee = new EventEmitter();
+				self.X.event_consumers[fid] = ee;
+				ee.on('event', function(ev)
+				{
+					console.log(ev);
+					if (ev.type === x11.event.DestroyNotify)
+					{
+					   self.X.DestroyWindow({window: fid});
+					} else if (ev.type == x11.event.ButtonPress) {
+						console.log("Drag start");
+						dragStart = { rootx: ev.root_x, rooty: ev.root_y, x: ev.event_x, y: ev.event_y, winX: winX, winY: winY };
+					} else if (ev.type == x11.event.ButtonRelease) {
+						console.log("Drag stop");
+						dragStart = null;
+					} else if (ev.type == x11.event.MotionNotify) {
+						console.log("Drag move");
+						winX = dragStart.winX + ev.root_x - dragStart.rootx;
+						winY = dragStart.winY + ev.root_y - dragStart.rooty;
+						self.X.ConfigureWindow({window:fid, value_mask: { X:winX, Y:winY}});
+					}
+				});
+				self.X.ChangeSaveSet({mode: 1, window: wid});
+				self.X.ReparentWindow({window: wid, parent:fid, x:1, y:21});
+				self.X.MapWindow({window:fid});
+				self.X.MapWindow({window:wid});
+			});
+		});
+	}
+
+
 }
 
 
-// A resolver that returns the first argument of the callback
-Future.prototype.resolver = function() {
-		return function(val) {
-			this.return(val);
-		}.bind(this);
-	},
+//exec("xterm");
 
 
 
-x11.createClient(function(display) {
-	Fiber(function() {
-	X = display.client;
-	var XF = Fiberize(X);
-	var p = Promise();
 
-	function AllocNamedColor(colorMap, name) {
-		var pro = Promise();
-		X.AllocNamedColor({cmap: colorMap, name: name}, pro);
-		return pro.get();
-	}
-	function GetAtomName(atom) {
-		var pro = Promise();
-		X.GetAtomName({atom: atom}, pro);
-		return pro.get();
-	}
+// "Main"
+var wm = new WindowManager();
+console.log("Constructor done");
 
-	var root = display.screen[0].root;
-	var white = display.screen[0].white_pixel;
-	var black = display.screen[0].black_pixel; 
-	var getAtomName = Future.wrap(X.GetAtomName, 1);
-	var allocNamedColor = Future.wrap(X.AllocNamedColor, 1);
-	var windows = {};
+//Object.create(WindowManager);
 
-	var name37 = GetAtomName(37);
-	var name39 = GetAtomName(39);
+Fiber(function() {
+	x11.createClient(wm.xinit);
+}).run();
 
-	console.log("White: ", white);
-	console.log("Atom 37: ", name37);
-	console.log("Atom 39: ", name39);
-	console.log("Root: ", root);
-	
-	var colorMap = display.screen[0].default_colormap;
-	console.log("Colormap: ", colorMap);
-	var colors = {
-		red: AllocNamedColor(colorMap, "Red"),
-		green: AllocNamedColor(colorMap, "Green"),
-		blue: AllocNamedColor(colorMap, "Blue"),
-		black: AllocNamedColor(colorMap, "Black"),
-		white: AllocNamedColor(colorMap, "White")
-	};
-	console.log("Colors: ", colors);
-
-	// Manage Root Window: listen for events, reparent any existing windows
-	var rootEventMask = x11.eventMask.SubstructureRedirect | x11.eventMask.SubstructureNotify | x11.eventMask.StructureNotify;
-	X.ChangeWindowAttributes( {window: root, value_mask: { EventMask: rootEventMask }} );
-
-
-	//$this->_execute("xsetroot \-mod 16 18 \-fg rgb:54/6/6 \-bg grey20");
-//	exec("xsetroot \-mod 16 18 \-fg rgb:54/6/6 \-bg grey20");
-	exec("xhello/hello");
-
-	X.on('event', function(ev) {
-		console.log('Received '+ev.name+' event: ', ev);
-		switch(ev.type) {
-			case x11.event.CreateNotify:
-				if (!ev.overrideRedirect) {
-				// 	foreach($this->_core->windows as $win) {
-				// 		if ($win->frame == $evt['window']) {
-				// 			echo "Configuring Frame {$evt['window']} for window $win->id\n";
-				// 			//$win->configureFrame($evt);
-				// 			return;
-				// 		}
-				// 	}
-					if (ev.parent == root) {
-						windows[ev.window] = new Window(X, ev);
-					}
-//					console.log("Windows: ", windows);
-				}
-				break;
-			case x11.event.MapRequest:
-//					console.log("Windows: ", windows);
-
-//				if (windows[ev.wid]) {
-					windows[ev.window].map();
-//				}
-				break;
-			case x11.event.MotionNotify:
-				  break;
-			case x11.event.Expose:
-				  draw(); break;
-			case x11.event.KeyPress:
-				  //console.log('keycode', ev);
-				  //console.log(X.keymap[ev.keycode]);
-				  // 111, 113, 114, 116, 65
-				  switch(ev.keycode) {            
-					  case up: rotateUp(); break;
-					  case down: rotateDown(); break;
-					  case left: moveLeft(); break;
-					  case right: moveRight(); break;
-					  case 65: drop(); break;
-				  }
-				  break;
-			default:
-				  console.log('default event', ev);
-		 }
-	});
-	console.log('End of createclient Fiber');
-	}).run();
-
-	console.log('End of createclient callback');
-});
+console.log("Called createClient done");
